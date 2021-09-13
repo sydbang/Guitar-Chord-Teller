16219698f21523ffd3886e45c00bc6b1854bfe31
sydbang <sydney.bang94@gmail.com>
2021-09-13 15:04:02 -0400

    Initial Commit

---
+++ GuitarChordTeller/GuitarChordTellerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarChordTellerApp.swift
+//  GuitarChordTeller
+//
+//  Created by Sunghee Bang on 2021-09-13.
+//
+
+import SwiftUI
+
+@main
+struct GuitarChordTellerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
