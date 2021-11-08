d2d583b44a9a702ecf419fc704eaa0bf0911da8e
sydbang <sydney.bang94@gmail.com>
2021-11-08 14:50:37 -0500

    - Create onboarding HowToView

---
+++ GuitarChordTeller/View/Onboarding/HowToView.swift
@@ -0,0 +1,21 @@
+//
+//  HowToView.swift
+//  GuitarChordTeller
+//
+//  Created by Sunghee Bang on 2021-11-08.
+//
+
+import SwiftUI
+
+struct HowToView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HowToView_Previews: PreviewProvider {
+    static var previews: some View {
+        HowToView()
+    }
+}
